f3ac7a2b4f09e40034ca59d2de579e256ea76b6a
riadkabir45 <riadkabir45@gmail.com>
2022-05-12 01:24:17 +0600

    feat: Character animation

--- Enemy.gd
+++ Enemy.gd
@@ -3,10 +3,12 @@ extends KinematicBody2D
 var speed = 2
 var nav = null
 var line = null
+var anim_player = null
 
 func _ready():
 	nav = $".."
 	line = $"../Line2D"
+	anim_player = $"AnimationPlayer"
 
 func focused_path(array):
 	pass
@@ -21,3 +23,14 @@ func _physics_process(_delta):
 	line.points = nav_array
 	
 	global_position = global_position.move_toward(nav_array[1],speed)
+	var dir = global_position.move_toward(nav_array[1],speed) - global_position
+	if dir == Vector2.ZERO:
+		anim_player.stop()
+	elif dir.x > 0:
+		anim_player.play("FollowRight")
+	elif dir.x < 0:
+		anim_player.play("FollowLeft")
+	elif dir.y > 0:
+		anim_player.play("FollowDown")
+	elif dir.y < 0:
+		anim_player.play("FollowUp")
--- Player.gd
+++ Player.gd
@@ -3,12 +3,24 @@ extends KinematicBody2D
 var dir = Vector2.ZERO
 var speed = 200
 var root = null
+var anim_player = null
 
 func _ready():
 	root = $".."
+	anim_player = $AnimationPlayer
 
 func _physics_process(delta):
 	dir.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	dir.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 	move_and_slide(dir*speed)
 	root.player_pos = global_position
+	if dir == Vector2.ZERO:
+		anim_player.stop()
+	elif dir.x > 0:
+		anim_player.play("Munch_Right")
+	elif dir.x < 0:
+		anim_player.play("Munch_Left")
+	elif dir.y > 0:
+		anim_player.play("Munch_Down")
+	elif dir.y < 0:
+		anim_player.play("Munch_Up")
